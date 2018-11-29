e804cc7c2f84e52164a5040af45ef5cce5c2a47a
noderme <33936521+noderme@users.noreply.github.com>
2018-11-29 10:13:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 //app = require('./app')('express');
 
 const mongoURl = '';
-//const mongoURl = 'mongodb://hareesh_tate:tate_007@ds141274.mlab.com:41274/tate_noder';
+
 const options = {
   reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
   useNewUrlParser:true
